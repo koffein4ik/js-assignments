6a752dc485835d3bbdbf9efa83226df85819329e
KoFFein4iK <koffein4ik@gmail.com>
2019-04-07 11:49:35 +0300

    date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   return (date % 4 === 0 ? date % 400 === 0 ? true : date % 100 === 0 ?  false : true : false);
 }
 
 
@@ -76,7 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   return new Date(endDate - startDate).toISOString().slice(11, 23);
 }
 
 
@@ -94,7 +94,13 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   var date1 = new Date(date);
+   var hours = date1.getHours();
+   var minutes = date1.getMinutes();
+   hours -= 3;
+   hours %= 12;
+   var degrees = (hours + (minutes /  60)) * 30 - minutes * 6; 
+   return (Math.abs(degrees) * Math.PI / 180);
 }
 
 
