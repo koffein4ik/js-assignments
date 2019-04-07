8a424b985edfc72f40b23ee554e4bf1d9df047a3
KoFFein4iK <koffein4ik@gmail.com>
2019-04-07 21:07:12 +0300

    string task upd

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -136,8 +136,8 @@ function isTriangle(a,b,c) {
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-     return ((((rect1.top + rect1.height) > rect2.top) && (rect2.top > rect1.top)) || (((rect2.top + rect2.height) > rect1.top) && (rect1.top > rect2.top)) ||
-             (((rect1.left + rect1.width) > rect2.left) && (rect2.left > rect1.left)) || (((rect2.left + rect2.width) > rect1.left) && (rect1.left > rect2.left)));
+     return ((((rect1.top + rect1.height) > rect2.top) && (rect2.top >= rect1.top)) || (((rect2.top + rect2.height) > rect1.top) && (rect1.top >= rect2.top)) ||
+             (((rect1.left + rect1.width) > rect2.left) && (rect2.left >= rect1.left)) || (((rect2.left + rect2.width) > rect1.left) && (rect1.left >= rect2.left)));
 }
 
 
